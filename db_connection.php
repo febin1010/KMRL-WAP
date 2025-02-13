1dac2f5c34bced913ef5bd30d46c7b2904d0a2b2
Febin cherian <78927280+febin1010@users.noreply.github.com>
2025-02-13 23:02:15 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'wap';
 $username = 'root';
-$password = 'KMRl@$#$2024#';
+$password = '************';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
